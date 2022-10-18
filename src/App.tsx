6b5d9ca59df8c8cb103f860709e9dbeaccf0713d
Adriel Ananta <adriel.ananta@gmail.com>
2022-10-18 14:24:56 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Tldraw, useFileSystem } from "@tldraw/tldraw";
+import { useMultiplayerState } from "./hooks/useMultiplayerState";
+import { roomID } from "./store";
 
 function App() {
+  const fileSystemEvents = useFileSystem();
+  const multiplayerEvents = useMultiplayerState(roomID);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tldraw">
+      <Tldraw 
+        autofocus
+        disableAssets
+        showPages={false} 
+        showMultiplayerMenu={false} 
+        {...multiplayerEvents}
+        {...fileSystemEvents}
+      />
     </div>
   );
 }
